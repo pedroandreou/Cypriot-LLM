b3949a1e5db006fa624391479a875c2b80daf989
Petros <pandreou01@gmail.com>
2023-09-04 22:48:59 +0300

    Fix creating two dirs for the same tokenized dataset - it's just one

--- src/_05_data_tokenizing/main.py
+++ src/_05_data_tokenizing/main.py
@@ -11,7 +11,9 @@ from src.utils.common_utils import (
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def tokenize_and_save_dataset(model_type, tokenizer_version, paths, block_size, key):
+def tokenize_and_save_dataset(
+    model_type, tokenizer_version, paths, block_size, key, out_path
+):
     echo_with_color(f"Tokenizing {key} files", color="bright_yellow")
     tokenized_dataset = TokenizedDataset(
         model_type=model_type,
@@ -21,27 +23,37 @@ def tokenize_and_save_dataset(model_type, tokenizer_version, paths, block_size, 
     )
 
     echo_with_color(f"Saving the tokenized {key} dataset...", color="bright_yellow")
-    dataset_dir_path_w_model_type = os.path.join(
-        curr_dir, "encodings", f"cy{model_type}"
-    )
-    dataset_dir_path_w_model_type_n_version = get_new_subdirectory_path(
-        dataset_dir_path_w_model_type, "encodings"
-    )
-    save_dataset(
-        dataset_dir_path_w_model_type_n_version, "tokenized", key, tokenized_dataset
-    )
+    save_dataset(out_path, "tokenized", key, tokenized_dataset)
 
 
 def main(model_type, tokenizer_version, block_size):
 
     train_paths, test_paths = PathSplitter.load_paths()
 
+    # Create a directory for the tokenized dataset
+    dataset_dir_path_w_model_type = os.path.join(
+        curr_dir, "encodings", f"cy{model_type}"
+    )
+    dataset_dir_path_w_model_type_n_version = get_new_subdirectory_path(
+        dataset_dir_path_w_model_type, "encodings"
+    )
+
     echo_with_color("Tokenizing files", color="bright_yellow")
     tokenize_and_save_dataset(
-        model_type, tokenizer_version, train_paths, block_size, "train"
+        model_type,
+        tokenizer_version,
+        train_paths,
+        block_size,
+        "train",
+        dataset_dir_path_w_model_type_n_version,
     )
     tokenize_and_save_dataset(
-        model_type, tokenizer_version, test_paths, block_size, "test"
+        model_type,
+        tokenizer_version,
+        test_paths,
+        block_size,
+        "test",
+        dataset_dir_path_w_model_type_n_version,
     )
 
 
